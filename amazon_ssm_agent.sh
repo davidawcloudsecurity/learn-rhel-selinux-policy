a0bbb83feeea40ef576eb388496192468895c9e0
David Aw <89636227+davidawcloudsecurity@users.noreply.github.com>
2024-05-14 22:16:38 +0800

    Update amazon_ssm_agent.sh

--- amazon_ssm_agent.sh
+++ amazon_ssm_agent.sh
@@ -1,25 +1,26 @@
 #!/bin/sh -e
 
-DIRNAME=`dirname $0`
-cd $DIRNAME
+DIRNAME=$(dirname "$0")
+cd "$DIRNAME"
 USAGE="$0 [ --update ]"
-if [ `id -u` != 0 ]; then
+if [ "$(id -u)" != 0 ]; then
     echo 'You must be root to run this script'
     exit 1
 fi
 
 if [ $# -eq 1 ]; then
     if [ "$1" = "--update" ] ; then
-        time=`ls -l --time-style="+%x %X" amazon_ssm_agent.te | awk '{ printf "%s %s", $6, $7 }'`
-        rules=`ausearch --start $time -m avc --raw -se amazon_ssm_agent`
-        if [ x"$rules" != "x" ] ; then
+        time=$(ls -l --time-style="+%x %X" amazon_ssm_agent.te | awk '{ printf "%s %s", $6, $7 }')
+        rules=$(ausearch --start "$time" -m avc --raw -se amazon_ssm_agent)
+        if [ "x$rules" != "x" ] ; then
             echo "Found avc's to update policy with"
             echo -e "$rules" | audit2allow -R
             echo "Do you want these changes added to policy [y/n]?"
-            read ANS
-            if [ "$ANS" = "y" -o "$ANS" = "Y" ] ; then
+            read -r ANS
+            if [ "$ANS" = "y" ] || [ "$ANS" = "Y" ] ; then
                 echo "Updating policy"
                 echo -e "$rules" | audit2allow -R >> amazon_ssm_agent.te
+                # Fall through and rebuild policy
             else
                 exit 0
             fi
@@ -28,11 +29,11 @@ if [ $# -eq 1 ]; then
             exit 0
         fi
     else
-        echo -e $USAGE
+        echo -e "$USAGE"
         exit 1
     fi
 elif [ $# -ge 2 ] ; then
-    echo -e $USAGE
+    echo -e "$USAGE"
     exit 1
 fi
 
